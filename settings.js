c245848c8f87e9321e12d16de4879f2eeb948bd8
Madison Gosselin <86690087+MadisonGosselin@users.noreply.github.com>
2024-07-26 18:37:11 +0000

    Fetch data from an API (3.2)

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 export default {
   // Get API key from https://home.openweathermap.org/users/sign_up
-  appid: [OpenWeatherAPIkey],
+  appid: "661e24759f5b423926b9f203b17718ef",
   units: "metric",
-  location: "carpinteria,ca,us",
+  location: "windsor,ca",
 };
