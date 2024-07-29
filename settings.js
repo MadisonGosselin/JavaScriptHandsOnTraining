f0aa35dc12b8603538d674aab66ed0d58b836fa0
Madison Gosselin <86690087+MadisonGosselin@users.noreply.github.com>
2024-07-29 12:26:53 +0000

    Add a button to change untis (3.4)

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 export default {
   // Get API key from https://home.openweathermap.org/users/sign_up
   appid: "661e24759f5b423926b9f203b17718ef",
-  units: "metric",
+  units: "imperial",
   location: "windsor,ca",
 };
